f539c83b5c2e885b57e6a4b285df0f04854df23f
CIPELKPR <CIPELKPR@AULA07PC11>
2019-05-13 09:39:16 -0300

    Agregar archivos de proyecto.

---
+++ PilaAvanzada.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico_4___Ejercicio_3
+{
+    class PilaAvanzada
+    {
+    }
+}
---
+++ TestPila.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico_4___Ejercicio_3
+{
+    class TestPila
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
