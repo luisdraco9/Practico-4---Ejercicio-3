7d8396821ea6c3ebffa9571b767d11c41df13278
CIPELKPR <CIPELKPR@AULA07PC11>
2019-05-13 09:47:25 -0300

    Pila

--- Pila.cs
+++ Pila.cs
@@ -6,7 +6,62 @@ using System.Threading.Tasks;
 
 namespace Practico_4___Ejercicio_3
 {
-    class Pila
+    public class Pila
     {
+        private int tope = -1;
+        private Disco[] listaDeDiscos;
+
+        public Pila(int tamano)
+        {
+            this.listaDeDiscos[] = listaDeDiscos[tamano];
+        }
+
+
+        public void Push(Disco disco)
+        {
+            if (IsFull == false)
+            {
+                tope++;
+                listaDeDiscos[tope] = disco;
+            }
+        }
+        public Disco Pop()
+        {
+            if (IsEmpty == false)
+            {
+                return listaDeDiscos[tope];
+                listaDeDiscos[tope] = null;
+                tope--;
+            }
+        }
+        public Disco Peek()
+        {
+            if (IsEmpty == false)
+            {
+                return listaDeDiscos[tope];
+            }
+        }
+        public bool IsEmpty()
+        {
+            if (tope == -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool IsFull()
+        {
+            if (tope == listaDeDiscos.Length - 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
